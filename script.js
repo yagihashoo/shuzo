c7166788cca524d027757927b1ad2c37d67367c9
Yu Yagihashi <yagihashoooooo@gmail.com>
2013-11-18 15:53:28 +0900

    タブをスペース2つに

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(function() {
-	shortcut.add("Meta+Alt+L", function() {
-		var path_to_mp3 = chrome.extension.getURL("./mp3.html")
-		var mp3_window = window.open(path_to_mp3, "mp3", "width=1,height=1,left=0,top=0,menubar=no,toolbar=no,location=no,status=no,resizable=no,scrollbars=no");
-	});
+  shortcut.add("Meta+Alt+L", function() {
+    var path_to_mp3 = chrome.extension.getURL("./mp3.html")
+    var mp3_window = window.open(path_to_mp3, "mp3", "width=1,height=1,left=0,top=0,menubar=no,toolbar=no,location=no,status=no,resizable=no,scrollbars=no");
+  });
 });
